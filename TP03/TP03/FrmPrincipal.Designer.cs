6fde6b896c034d7e20a22be23b1a4476ccaa9c48
Robson Lucas Silveira Mello <robson.mello3@etec.sp.gov.br>
2019-08-21 20:53:06 -0300

    TP03

---
+++ TP03/TP03/FrmPrincipal.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace TP03
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlIcosaedro = new System.Windows.Forms.Panel();
+            this.pcbIcosaedro = new System.Windows.Forms.PictureBox();
+            this.pnlDodecaedro = new System.Windows.Forms.Panel();
+            this.pcbDodecaedro = new System.Windows.Forms.PictureBox();
+            this.pnlOctaedro = new System.Windows.Forms.Panel();
+            this.pcbOctaedro = new System.Windows.Forms.PictureBox();
+            this.pnlCubo = new System.Windows.Forms.Panel();
+            this.pcbCubo = new System.Windows.Forms.PictureBox();
+            this.pnlTetraedro = new System.Windows.Forms.Panel();
+            this.pcbTetraedo = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.pnlIcosaedro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbIcosaedro)).BeginInit();
+            this.pnlDodecaedro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDodecaedro)).BeginInit();
+            this.pnlOctaedro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOctaedro)).BeginInit();
+            this.pnlCubo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCubo)).BeginInit();
+            this.pnlTetraedro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbTetraedo)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(200)))), ((int)(((byte)(100)))));
+            this.panel1.Controls.Add(this.pnlIcosaedro);
+            this.panel1.Controls.Add(this.pnlDodecaedro);
+            this.panel1.Controls.Add(this.pnlOctaedro);
+            this.panel1.Controls.Add(this.pnlCubo);
+            this.panel1.Controls.Add(this.pnlTetraedro);
+            this.panel1.Location = new System.Drawing.Point(-1, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 155);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlIcosaedro
+            // 
+            this.pnlIcosaedro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlIcosaedro.BackgroundImage")));
+            this.pnlIcosaedro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlIcosaedro.Controls.Add(this.pcbIcosaedro);
+            this.pnlIcosaedro.Location = new System.Drawing.Point(176, 79);
+            this.pnlIcosaedro.Name = "pnlIcosaedro";
+            this.pnlIcosaedro.Size = new System.Drawing.Size(73, 73);
+            this.pnlIcosaedro.TabIndex = 1;
+            // 
+            // pcbIcosaedro
+            // 
+            this.pcbIcosaedro.Image = global::TP03.Properties.Resources.icosaedro;
+            this.pcbIcosaedro.Location = new System.Drawing.Point(4, 4);
+            this.pcbIcosaedro.Name = "pcbIcosaedro";
+            this.pcbIcosaedro.Size = new System.Drawing.Size(65, 65);
+            this.pcbIcosaedro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbIcosaedro.TabIndex = 0;
+            this.pcbIcosaedro.TabStop = false;
+            // 
+            // pnlDodecaedro
+            // 
+            this.pnlDodecaedro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlDodecaedro.BackgroundImage")));
+            this.pnlDodecaedro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlDodecaedro.Controls.Add(this.pcbDodecaedro);
+            this.pnlDodecaedro.Location = new System.Drawing.Point(97, 78);
+            this.pnlDodecaedro.Name = "pnlDodecaedro";
+            this.pnlDodecaedro.Size = new System.Drawing.Size(73, 73);
+            this.pnlDodecaedro.TabIndex = 1;
+            // 
+            // pcbDodecaedro
+            // 
+            this.pcbDodecaedro.Image = global::TP03.Properties.Resources.dodecaedro;
+            this.pcbDodecaedro.Location = new System.Drawing.Point(4, 4);
+            this.pcbDodecaedro.Name = "pcbDodecaedro";
+            this.pcbDodecaedro.Size = new System.Drawing.Size(65, 65);
+            this.pcbDodecaedro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbDodecaedro.TabIndex = 0;
+            this.pcbDodecaedro.TabStop = false;
+            // 
+            // pnlOctaedro
+            // 
+            this.pnlOctaedro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlOctaedro.BackgroundImage")));
+            this.pnlOctaedro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlOctaedro.Controls.Add(this.pcbOctaedro);
+            this.pnlOctaedro.Location = new System.Drawing.Point(18, 78);
+            this.pnlOctaedro.Name = "pnlOctaedro";
+            this.pnlOctaedro.Size = new System.Drawing.Size(73, 73);
+            this.pnlOctaedro.TabIndex = 1;
+            // 
+            // pcbOctaedro
+            // 
+            this.pcbOctaedro.Image = global::TP03.Properties.Resources.octaedro;
+            this.pcbOctaedro.Location = new System.Drawing.Point(4, 4);
+            this.pcbOctaedro.Name = "pcbOctaedro";
+            this.pcbOctaedro.Size = new System.Drawing.Size(65, 65);
+            this.pcbOctaedro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbOctaedro.TabIndex = 0;
+            this.pcbOctaedro.TabStop = false;
+            // 
+            // pnlCubo
+            // 
+            this.pnlCubo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlCubo.BackgroundImage")));
+            this.pnlCubo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlCubo.Controls.Add(this.pcbCubo);
+            this.pnlCubo.Location = new System.Drawing.Point(143, 3);
+            this.pnlCubo.Name = "pnlCubo";
+            this.pnlCubo.Size = new System.Drawing.Size(73, 73);
+            this.pnlCubo.TabIndex = 1;
+            // 
+            // pcbCubo
+            // 
+            this.pcbCubo.Image = global::TP03.Properties.Resources.cubo;
+            this.pcbCubo.Location = new System.Drawing.Point(4, 4);
+            this.pcbCubo.Name = "pcbCubo";
+            this.pcbCubo.Size = new System.Drawing.Size(65, 65);
+            this.pcbCubo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbCubo.TabIndex = 0;
+            this.pcbCubo.TabStop = false;
+            // 
+            // pnlTetraedro
+            // 
+            this.pnlTetraedro.BackgroundImage = global::TP03.Properties.Resources.arredondado2;
+            this.pnlTetraedro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlTetraedro.Controls.Add(this.pcbTetraedo);
+            this.pnlTetraedro.Location = new System.Drawing.Point(54, 3);
+            this.pnlTetraedro.Name = "pnlTetraedro";
+            this.pnlTetraedro.Size = new System.Drawing.Size(73, 73);
+            this.pnlTetraedro.TabIndex = 0;
+            // 
+            // pcbTetraedo
+            // 
+            this.pcbTetraedo.Image = global::TP03.Properties.Resources.tetraedo;
+            this.pcbTetraedo.Location = new System.Drawing.Point(4, 4);
+            this.pcbTetraedo.Name = "pcbTetraedo";
+            this.pcbTetraedo.Size = new System.Drawing.Size(65, 65);
+            this.pcbTetraedo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbTetraedo.TabIndex = 0;
+            this.pcbTetraedo.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(71)))), ((int)(((byte)(36)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(-1, -3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(268, 37);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-2, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Áreas e Volumes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 403);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.pnlIcosaedro.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbIcosaedro)).EndInit();
+            this.pnlDodecaedro.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDodecaedro)).EndInit();
+            this.pnlOctaedro.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOctaedro)).EndInit();
+            this.pnlCubo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCubo)).EndInit();
+            this.pnlTetraedro.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbTetraedo)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlIcosaedro;
+        private System.Windows.Forms.PictureBox pcbIcosaedro;
+        private System.Windows.Forms.Panel pnlDodecaedro;
+        private System.Windows.Forms.PictureBox pcbDodecaedro;
+        private System.Windows.Forms.Panel pnlOctaedro;
+        private System.Windows.Forms.PictureBox pcbOctaedro;
+        private System.Windows.Forms.Panel pnlCubo;
+        private System.Windows.Forms.PictureBox pcbCubo;
+        private System.Windows.Forms.Panel pnlTetraedro;
+        private System.Windows.Forms.PictureBox pcbTetraedo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ TP03/TP03/frmPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP03
+{
+    public partial class FrmPrincipal : Form
+    {
+        private Panel[] panels;
+        public FrmPrincipal()
+        {
+            panels = new Panel[5];
+            panels[0] = pnlTetraedro;
+            panels[1] = pnlCubo;
+            panels[2] = pnlOctaedro;
+            panels[3] = pnlDodecaedro;
+            panels[4] = pnlIcosaedro;
+            InitializeComponent();
+            this.selecionarPcbAtual();
+        }
+
+        public void selecionarPcbAtual()
+        {
+            
+            foreach (var panel in panels)
+            {
+
+                panel.BackgroundImage = Properties.Resources.arredondadopreenchido;
+            }
+        }
+    }
+}
