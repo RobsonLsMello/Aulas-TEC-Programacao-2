2c07d7d5cc1b5eb782af35235ab4b8947f68c0cf
Robson Lucas Silveira Mello <robson.mello3@etec.sp.gov.br>
2019-08-28 20:15:58 -0300

    tp 04

---
+++ TP04/TP04/FrmPrincipal.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TP04
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxPressao = new System.Windows.Forms.PictureBox();
+            this.gbxDados = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPressaoInicial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTemperaturaInicial = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbxResultados = new System.Windows.Forms.ListBox();
+            this.tmrUpdate = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPressao)).BeginInit();
+            this.gbxDados.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbxPressao
+            // 
+            this.pbxPressao.Image = global::TP04.Properties.Resources.CH1;
+            this.pbxPressao.Location = new System.Drawing.Point(20, 16);
+            this.pbxPressao.Name = "pbxPressao";
+            this.pbxPressao.Size = new System.Drawing.Size(122, 115);
+            this.pbxPressao.TabIndex = 0;
+            this.pbxPressao.TabStop = false;
+            // 
+            // gbxDados
+            // 
+            this.gbxDados.Controls.Add(this.label4);
+            this.gbxDados.Controls.Add(this.label3);
+            this.gbxDados.Controls.Add(this.label2);
+            this.gbxDados.Controls.Add(this.txtPressaoInicial);
+            this.gbxDados.Controls.Add(this.label1);
+            this.gbxDados.Controls.Add(this.txtTemperaturaInicial);
+            this.gbxDados.Location = new System.Drawing.Point(169, 12);
+            this.gbxDados.Name = "gbxDados";
+            this.gbxDados.Size = new System.Drawing.Size(212, 118);
+            this.gbxDados.TabIndex = 1;
+            this.gbxDados.TabStop = false;
+            this.gbxDados.Text = "Dados:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "(atm)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "(ºC)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pressão Inicial:";
+            // 
+            // txtPressaoInicial
+            // 
+            this.txtPressaoInicial.Location = new System.Drawing.Point(9, 80);
+            this.txtPressaoInicial.Name = "txtPressaoInicial";
+            this.txtPressaoInicial.Size = new System.Drawing.Size(161, 20);
+            this.txtPressaoInicial.TabIndex = 2;
+            this.txtPressaoInicial.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPressaoInicial_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Temperatura Inicial:";
+            // 
+            // txtTemperaturaInicial
+            // 
+            this.txtTemperaturaInicial.Location = new System.Drawing.Point(9, 38);
+            this.txtTemperaturaInicial.Name = "txtTemperaturaInicial";
+            this.txtTemperaturaInicial.Size = new System.Drawing.Size(161, 20);
+            this.txtTemperaturaInicial.TabIndex = 0;
+            this.txtTemperaturaInicial.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTemperaturaInicial_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSair);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Location = new System.Drawing.Point(20, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(121, 182);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controle";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(11, 124);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(99, 43);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(11, 75);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(99, 43);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(11, 26);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(99, 43);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbxResultados
+            // 
+            this.lbxResultados.FormattingEnabled = true;
+            this.lbxResultados.Location = new System.Drawing.Point(25, 26);
+            this.lbxResultados.Name = "lbxResultados";
+            this.lbxResultados.Size = new System.Drawing.Size(164, 147);
+            this.lbxResultados.TabIndex = 3;
+            // 
+            // tmrUpdate
+            // 
+            this.tmrUpdate.Enabled = true;
+            this.tmrUpdate.Interval = 50;
+            this.tmrUpdate.Tick += new System.EventHandler(this.tmrUpdate_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbxResultados);
+            this.groupBox2.Location = new System.Drawing.Point(169, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(211, 181);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados (m³):";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 339);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxDados);
+            this.Controls.Add(this.pbxPressao);
+            this.Name = "frmPrincipal";
+            this.Text = "Ensaio de Líquidos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPressao)).EndInit();
+            this.gbxDados.ResumeLayout(false);
+            this.gbxDados.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxPressao;
+        private System.Windows.Forms.GroupBox gbxDados;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPressaoInicial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTemperaturaInicial;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lbxResultados;
+        private System.Windows.Forms.Timer tmrUpdate;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ TP04/TP04/FrmPrincipal.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TP04.BLL;
+
+namespace TP04
+{
+    public partial class frmPrincipal : Form
+    {
+        #region variáveis globais
+        private int contadorGarrafa;
+        private bool podeAnimarGarrafa;
+        #endregion
+        public frmPrincipal()
+        {
+            podeAnimarGarrafa = false;
+            InitializeComponent();
+        }
+        #region Função para animar as fotos
+        /*
+         *  Função responsável por fazer a animação com base no tipo e nos sprites, e retornar o novo indíce
+         */
+        public int animarFoto(TipoAnimacao tipoAnimacao, int sprite, int numeroSpriteMax, string diretorio = "")
+        {
+            int indice = sprite % numeroSpriteMax + 1;
+            if (tipoAnimacao == TipoAnimacao.garrafa)
+            {
+                switch (indice)
+                {
+                    case 1:
+                        pbxPressao.Image = Properties.Resources.CH1;
+                        break;
+                    case 2:
+                        pbxPressao.Image = Properties.Resources.CH2;
+                        break;
+                    case 3:
+                        pbxPressao.Image = Properties.Resources.CH3;
+                        break;
+                    case 4:
+                        pbxPressao.Image = Properties.Resources.CH4;
+                        break;
+                    case 5:
+                        pbxPressao.Image = Properties.Resources.CH5;
+                        break;
+                    case 6:
+                        pbxPressao.Image = Properties.Resources.CH6;
+                        break;
+                    case 7:
+                        pbxPressao.Image = Properties.Resources.CH7;
+                        break;
+                    case 8:
+                        pbxPressao.Image = Properties.Resources.CH8;
+                        break;
+                    case 9:
+                        pbxPressao.Image = Properties.Resources.CH9;
+                        break;
+                }
+            }
+            
+            return indice;
+        }
+        #endregion
+        #region Calcular todos os 10 volumes
+        /*
+         * Função responsável por calcular todos os 10 volumes em um laço de repetição e devolver em forma de lista
+         */
+        public List<double> calcularVolumes(double pressao, double temperatura)
+        {
+            List<double> volumes = new List<double>();
+            for (int i = 0; i < 10; i++)
+            {
+                double volume = ((82 * Math.Pow(10, -3))/pressao)*((temperatura + i*2) + 273);
+                volume = Math.Round(volume, 2);
+                volumes.Add(volume);
+            }
+            podeAnimarGarrafa = true;
+            return volumes;
+        }
+        #endregion
+        #region botão calcular
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var volumesCalculados = calcularVolumes(double.Parse(txtPressaoInicial.Text), double.Parse(txtTemperaturaInicial.Text));
+                lbxResultados.DataSource = volumesCalculados;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, digite números", "Campo numérico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro desconhecido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+        #region Timer Update
+        private void tmrUpdate_Tick(object sender, EventArgs e)
+        {
+            if (podeAnimarGarrafa)
+            {
+                contadorGarrafa = animarFoto(TipoAnimacao.garrafa, contadorGarrafa, 9);
+            }
+        }
+        #endregion
+        #region Validar textBox para números reais
+        /*
+         * Função responsável por válidar conteudo das textoBox, bloqueando a digitação de qualquer coisa que não
+         * represente um número real, podendo bloquear os números negativo
+         */
+        private void validarTextBoxParaNumerosReais(TextBox textBoxAlvo, KeyPressEventArgs e, bool podeNegativo = true)
+        {
+            if (e.KeyChar == '.' || e.KeyChar == ',')
+            {
+                //troca o . pela virgula
+                e.KeyChar = '.';
+                //Verifica se já existe alguma vírgula na string
+                if (textBoxAlvo.Text.Contains("."))
+                {
+                    //bloqueia a digitação do próximo caracter inválido
+                    e.Handled = true;
+                }
+            }
+            else
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    if ((e.KeyChar == '-') && podeNegativo)
+                    {
+                        if (textBoxAlvo.Text.Length != 0)
+                        {
+                            //bloqueia a digitação do próximo caracter inválido
+                            e.Handled = true;
+                        }
+                    }
+                    else
+                    {
+                        //bloqueia a digitação do próximo caracter inválido
+                        e.Handled = true;
+                    }
+
+                }
+            }
+
+
+        }
+        #endregion
+        #region Pressionar teclado no txtTemperaturaInicial
+        private void txtTemperaturaInicial_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            validarTextBoxParaNumerosReais(txtTemperaturaInicial, e);
+        }
+        #endregion
+        #region Pressionar teclado no txtPressaoInicial
+        private void txtPressaoInicial_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            validarTextBoxParaNumerosReais(txtPressaoInicial, e);
+        }
+        #endregion
+    }
+}
